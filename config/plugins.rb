049ad695a7172789941d4888f229f31bce9656fb
Alec McGail <amcgail2@gmail.com>
2024-11-25 16:24:05 -0600

    fixed perhaps

--- config/plugins.rb
+++
@@ -1,8 +0,0 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.19.2"
-
-set :application, "PRMERS"
-set :repo_url, "git@github.com:amcgail/discourse-PRMERS.git"
-
-# set the directory for the plugins
-set :deploy_to, "/home/ubuntu/PRMERS-plugins"
