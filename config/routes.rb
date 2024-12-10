b842209f54b6f9a6e53023ef4cda1550d5ca1f0a
Alec McGail <amcgail2@gmail.com>
2024-12-10 15:48:06 -0600

    hmm what

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
-PRMERSPlugin::Engine.routes.draw do
+PrmersPlugin::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
+
+  root to: "examples#index"
+
+  # Example of a route that requires authentication
+  # get "/secure" => "examples#secure"
+
+  # Example of a route that requires admin privileges
+  # get "/admin" => "examples#admin"
+
+  # Example of a route that requires admin privileges
+  # get "/moderator" => "examples#moderator"
 end
 
-Discourse::Application.routes.draw { mount ::PRMERSPlugin::Engine, at: "prmers" }
+Discourse::Application.routes.draw { mount ::PrmersPlugin::Engine, at: "prmers" }
