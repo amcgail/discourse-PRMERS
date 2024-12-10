2b9f7e242ffbdba84514e426b778ad36f0deb69f
Alec McGail <amcgail2@gmail.com>
2024-12-10 15:39:04 -0600

    fiddling with controller and beefing up documentation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-PRMERSPlugin::Engine.routes.draw do
+PrmersPlugin::Engine.routes.draw do
   get "/examples" => "examples#index"
   # define routes here
 end
 
-Discourse::Application.routes.draw { mount ::PRMERSPlugin::Engine, at: "prmers" }
+Discourse::Application.routes.draw { mount ::PrmersPlugin::Engine, at: "prmers" }
--- plugin.rb
+++ plugin.rb
@@ -8,11 +8,11 @@
 
 enabled_site_setting :plugin_name_enabled
 
-module ::PRMERSPlugin
+module ::PrmersPlugin
   PLUGIN_NAME = "discourse-PRMERS"
 end
 
-require_relative "lib/PRMERS_plugin/engine"
+require_relative "lib/prmers_plugin/engine"
 
 after_initialize do
   # Code which should run after Rails has finished booting
